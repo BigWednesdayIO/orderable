95f0accc2b444a9b144c5a49d9c18dadaabb1e12
Michael Strutt <michael@mstrutt.co.uk>
2015-11-06 15:30:20 +0000

    switching to dev api

--- app/assets/js/constants/api.js
+++ app/assets/js/constants/api.js
@@ -1,14 +1,13 @@
 function APIConstant () {
 	var API = {};
 
-	API.base = '/api/';
-	API.search = API.base + 'v1/search';
+	API.base = 'https://api.bigwednesday.io/1/';
+	API.base = 'https://nzgrud6b.bigwednesday.io/1/';
 
-	API.navigation = '/mocks/navigation.json';
+	API.navigation = 'mocks/navigation.json';
 
-	API.search_base = 'https://api.bigwednesday.io/1/search/indexes/';
-	API.search = API.search_base + 'crateful-products/query';
-	API.search_suggestions = API.search_base + 'crateful-suggestions/query'
+	API.search = API.base + 'indexes/crateful-products/query';
+	API.search_suggestions = API.base + 'indexes/crateful-suggestions/query'
 
 	API.categories = 'https://raw.githubusercontent.com/BigWednesdayIO/categories-api/master/categories.json';
 
--- app/assets/js/interceptors/authorization.js
+++ app/assets/js/interceptors/authorization.js
@@ -1,7 +1,7 @@
 function AuthorizationInterceptor (API) {
 	return {
 		request: function(config) {
-			if (config.url.match(API.search_base)) {
+			if (config.url.match(API.base)) {
 				config.headers['Authorization'] = 'Bearer NG0TuV~u2ni#BP|';
 			}
 			return config;
