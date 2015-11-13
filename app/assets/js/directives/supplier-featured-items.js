9e0612fad0e137074ad38b17713ff0d9f0025cc5
Michael Strutt <michael@mstrutt.co.uk>
2015-11-13 16:24:36 +0000

    featured product carousel JS

--- app/assets/js/directives/supplier-featured-items.js
+++ app/assets/js/directives/supplier-featured-items.js
@@ -1,4 +1,4 @@
-function SupplierFeaturedItemsDirective () {
+function SupplierFeaturedItemsDirective ($window) {
 	return {
 		restrict: 'EA',
 		scope: {
@@ -6,17 +6,61 @@ function SupplierFeaturedItemsDirective () {
 			hits: '=',
 			buttonText: '@'
 		},
-		controller: function(searchService, suppliersService) {
-			var vm = this;
+		controller: function($scope, searchService, suppliersService) {
+			$scope.href = searchService.applyRefinementToUrl('supplier', $scope.supplier);
 
-			vm.href = searchService.applyRefinementToUrl('supplier', vm.supplier);
-
-			vm.logo = suppliersService.getLogoForSupplier(vm.supplier);
+			$scope.logo = suppliersService.getLogoForSupplier($scope.supplier);
 		},
-		controllerAs: 'vm',
-		bindToController: true,
 		templateUrl: 'views/partials/supplier-featured-items.html',
-		replace: true
+		replace: true,
+		link: function(scope, element) {
+			var viewport = element[0].querySelector('#viewport'),
+				thumbWidth = 212,
+				container = viewport.querySelector('#items'),
+				containerWidth = thumbWidth * scope.hits.length,
+				offset = 0,
+				viewportWidth,
+				visibleThumbs;
+
+			function calculateWidths () {
+				viewportWidth = viewport.offsetWidth;
+				visibleThumbs = Math.floor(viewportWidth / thumbWidth)
+			}
+
+			function slide () {
+				container.style.transform = 'translate(' + offset + 'px, 0)';
+				scope.farLeft = offset === 0;
+				scope.farRight = containerWidth + offset <= viewportWidth;
+			}
+
+			scope.slideRight = function($event) {
+				var newOffset;
+				$event.preventDefault();
+				newOffset = offset - (visibleThumbs * thumbWidth);
+				if (newOffset * -1 >= containerWidth) {
+					return;
+				}
+				offset = newOffset;
+				slide();
+			};
+
+			scope.slideLeft = function($event) {
+				var newOffset;
+				$event.preventDefault();
+				newOffset = offset + (visibleThumbs * thumbWidth);
+				offset = (newOffset > 0) ? 0 : newOffset;
+				slide();
+			};
+
+			$window.addEventListener('resize', calculateWidths);
+			scope.$on('$destroy', function() {
+				$window.removeEventListener('resize', calculateWidths);
+			})
+
+			// Setting it up at 0
+			calculateWidths();
+			slide();
+		}
 	};
 }
 
