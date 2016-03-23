5da800c66e10be7580a6f913036b260a5302ff3a
Michael Strutt <michael@mstrutt.co.uk>
2016-03-23 15:34:04 +0000

    saving default_sign_for attribute

--- app/assets/js/controllers/account-settings.js
+++ app/assets/js/controllers/account-settings.js
@@ -1,16 +1,28 @@
-function AccountSettingsController ($mdToast, suppliersService, membershipsService) {
+function AccountSettingsController ($mdToast, $q, suppliersService, membershipsService, customerService, customerInfo) {
 	var vm = this;
+
+	vm.defaultSignFor = customerInfo.default_sign_for || false;
 
 	vm.suppliers = suppliersService.getCurrentSuppliers();
 
 	vm.saveChanges = function() {
-		return membershipsService
+		var updateMemberships = membershipsService
 			.updateMemberships(vm.supplierMemberships.map(function(supplierMembership) {
 				return {
 					supplier_id: supplierMembership.supplier.id,
 					membership_number: supplierMembership.number
 				};
-			}))
+			}));
+
+		var updateDefaultSignFor = customerService
+			.updateInfo({
+				default_sign_for: vm.defaultSignFor
+			});
+
+		return $q.all([
+			updateMemberships,
+			updateDefaultSignFor
+		])
 			.then(function() {
 				return $mdToast.show(
 					$mdToast.simple()
@@ -21,6 +33,10 @@ function AccountSettingsController ($mdToast, suppliersService, membershipsServi
 }
 
 AccountSettingsController.resolve = /* @ngInject */ {
+	customerInfo: function(customerService) {
+		return customerService
+			.getUpToDateInfo();
+	},
 	requiresSignIn: function(authorizationService) {
 		return authorizationService
 			.requireSignIn();
